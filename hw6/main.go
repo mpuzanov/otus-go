c0acfc5c202dda65bf993dfff9944b57baebaeba
Mikhail Puzanov <mpuzanov@mail.ru>
2020-01-19 12:59:18 +0400

    Добавил тесты

--- hw6/main.go
+++ hw6/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	//	_ "hw6/internal/app/gocopy"
+	cp "hw6/internal/app/gocopy"
+	"os"
 )
 
+var (
+	from, to      string
+	offset, limit int
+)
+
+func init() {
+	flag.StringVar(&from, "from", "", "file copy from")
+	flag.StringVar(&to, "to", "", "file copy to")
+	flag.IntVar(&offset, "offset", 0, "offset in input file")
+	flag.IntVar(&limit, "limit", 0, "limit copy file")
+}
+
 func main() {
-	fmt.Println("Программа копирования файлов")
+	flag.Parse()
+	if from == "" || to == "" {
+		flag.PrintDefaults()
+		return
+	}
+	if err := cp.Copy(from, to, limit, offset); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	} else {
+		fmt.Println("copied successfully")
+	}
+
 }
