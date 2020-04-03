4e85f68356c6596dbcc127a7d4781effef388b8c
Mikhail Puzanov <mpuzanov@mail.ru>
2020-04-03 13:35:43 +0400

    доработка дз по докеру

--- calendar/build/docker-down.sh
+++ calendar/build/docker-down.sh
@@ -2,7 +2,7 @@
 docker-compose  --file build/docker-compose.yml down #--remove-orphans
 
 #docker image rm calendar_db:latest --force
-docker image rm calendar_db --force
+#docker image rm calendar_db --force
 docker image rm calendar_db_test --force
 docker image rm calendar_sender --force
 docker image rm calendar_scheduler --force
--- calendar/build/docker-up.sh
+++ calendar/build/docker-up.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export TZ=Europe/Samara
+export POSTGRES_USER=postgres
+export POSTGRES_DB=pg_calendar_test
+export POSTGRES_PASSWORD=12345
 
 #docker build -t calendar_db:latest -f ./build/db/Dockerfile .
 #docker build -t calendar_db_test:latest -f ./build/db-test/Dockerfile .
 
-#docker-compose -f build/docker-compose.yml up --detach
-
 docker-compose -f build/docker-compose.yml up --build --detach #--remove-orphans
-#docker-compose -f build/docker-compose2.yml up --build --detach
+#docker-compose -f build/docker-compose2.yml up --detach
