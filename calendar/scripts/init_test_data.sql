42ac30f3e5041700d5e96bd8ff58c90445befbe4
Mikhail Puzanov <mpuzanov@mail.ru>
2020-04-04 14:26:40 +0400

    удалил не используемую папку

--- calendar/scripts/init_test_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public.events("header", "text", "start_time", "end_time", "user_id", "reminder_before") VALUES 
-('event 1', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '1 hour' ),
-('event 2', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '0' ),
-('event 3', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '0' ),
-('event 4', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '30 minutes' ),
-('event 5', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '30 minutes' ),
-('event 6', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '2 hour' ),
-('event 7', 'описание события','2020-04-01 09:00:00'::timestamp , '2020-04-01 10:30:00'::timestamp, 'User1', interval '1 day' )
-;
